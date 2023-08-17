a4b3eed30c38d5a3b1d18226819a798a2cb26de1
csillagk <katalin.csillag@emergence-engineering.com>
2023-08-17 10:25:40 +0200

    draggable block done

--- apps/emergence-engineering-lexical-examples/app/editor/page.tsx
+++ apps/emergence-engineering-lexical-examples/app/editor/page.tsx
@@ -14,19 +14,20 @@ import "lexical-link-preview-plugin/dist/styles/style.css";
 
 import { exampleTheme } from "./exampleTheme";
 import { useState } from "react";
+import DraggableBlockPlugin from "@/app/editor/(draggableBlock)/DraggableBlockPlugin";
 
 function onError(error: any) {
   console.error("lexical.onError", { error });
 }
 
-function Editor() {
-  const initialConfig = {
-    namespace: "MyEditor",
-    theme: { ...exampleTheme, linkPreviewContainer: "linkPreviewContainer" },
-    onError,
-    nodes: [LinkPreviewNode],
-  };
+const initialConfig = {
+  namespace: "MyEditor",
+  theme: { ...exampleTheme, linkPreviewContainer: "linkPreviewContainer" },
+  onError,
+  nodes: [LinkPreviewNode],
+};
 
+function Editor() {
   const [floatingAnchorElem, setFloatingAnchorElem] =
     useState<HTMLDivElement | null>(null);
 
@@ -39,62 +40,65 @@ function Editor() {
   return (
     <div style={{ padding: "1rem" }}>
       <LexicalComposer initialConfig={initialConfig}>
-        {/*{floatingAnchorElem && (*/}
-        {/*    <DraggableBlockPlugin anchorElem={floatingAnchorElem} />*/}
-        {/*)}*/}
-        <LinkPreviewPlugin
-          showLink={false}
-          fetchDataForPreview={(link: string) => {
-            return fetch("api/preview", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
+        <div style={{ position: "relative" }}>
+          {floatingAnchorElem && (
+            <DraggableBlockPlugin anchorElem={floatingAnchorElem} />
+          )}
+
+          <LinkPreviewPlugin
+            showLink={false}
+            fetchDataForPreview={(link: string) => {
+              return fetch("api/preview", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ link }),
+              })
+                .then((res) => res.json())
+                .then((res) => {
+                  return res.data;
+                });
+            }}
+          />
+          <SlashMenuPlugin
+            menuElements={[
+              {
+                id: "1",
+                label: "First",
+                type: "command",
               },
-              body: JSON.stringify({ link }),
-            })
-              .then((res) => res.json())
-              .then((res) => {
-                return res.data;
-              });
-          }}
-        />
-        <SlashMenuPlugin
-          menuElements={[
-            {
-              id: "1",
-              label: "First",
-              type: "command",
-            },
-            {
-              id: "2",
-              label: "Second",
-              type: "command",
-            },
-            {
-              id: "3",
-              label: "Third",
-              type: "command",
-            },
-          ]}
-        />
-        <RichTextPlugin
-          placeholder={null}
-          contentEditable={
-            <div className="editor-scroller">
-              <div ref={onRef} className={"editor"}>
-                <ContentEditable
-                  style={{
-                    padding: "10px",
-                    border: "1px solid #ccc",
-                    borderRadius: "5px",
-                    minHeight: "500px",
-                  }}
-                />
+              {
+                id: "2",
+                label: "Second",
+                type: "command",
+              },
+              {
+                id: "3",
+                label: "Third",
+                type: "command",
+              },
+            ]}
+          />
+          <RichTextPlugin
+            placeholder={null}
+            contentEditable={
+              <div className="editor-scroller">
+                <div ref={onRef} className={"editor"}>
+                  <ContentEditable
+                    style={{
+                      padding: "10px 24px",
+                      border: "1px solid #ccc",
+                      borderRadius: "5px",
+                      minHeight: "500px",
+                    }}
+                  />
+                </div>
               </div>
-            </div>
-          }
-          ErrorBoundary={LexicalErrorBoundary}
-        />
+            }
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+        </div>
       </LexicalComposer>
     </div>
   );
